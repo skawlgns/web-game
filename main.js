328bc1cb68915b3b30fd9b372b6747829f755847
skawlgns <skawlgns@kakao.com>
2022-01-03 11:04:42 +0900

    'timer

--- main.js
+++ main.js
@@ -6,10 +6,110 @@ const field = document.querySelector(".game__field");
 const gameBtn = document.querySelector(".game__button");
 const gameTimer = document.querySelector(".game__timer");
 const fieldRect = field.getBoundingClientRect();
-
+const popUp = document.querySelector(".pop-up");
+const popUpMsg = document.querySelector(".pop-up__message");
+const Refresh = document.querySelector(".pop-up__refresh");
 const picked = new Audio("./sound/o.mp3");
+let score = 1;
+let started = false;
+let timer = undefined;
+gameBtn.addEventListener("click", () => {
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+});
+
+field.addEventListener("click", catchCharacter);
+
+Refresh.addEventListener("click", () => {
+  startGame();
+  hidePopUp();
+});
+
+function startGame() {
+  started = true;
+  init();
+  pauseButton();
+  hideGameButton();
+  showTimer();
+  startTimer();
+  hidePopUp();
+}
+
+function stopGame() {
+  started = false;
+  showPopUp();
+  hideGameButton();
+}
+
+function finishGame(win) {
+  started = false;
+  stopGameTimer();
+  showPopUp(win ? "YOU LOSE..." : "CONGRATULATION!!");
+}
+
+function startTimer() {
+  let playTime = 1;
+
+  updateTimer(playTime);
+  timer = setInterval(() => {
+    if (playTime <= 0) {
+      clearInterval(timer);
+      finishGame(score);
+      return;
+    }
+    updateTimer(--playTime);
+  }, 1000);
+}
+
+function catchCharacter(e) {
+  if (!started) {
+    return;
+  }
+  if (e.target.matches(".item")) {
+    e.target.remove();
+    --score;
+    if (score === 0) {
+      finishGame(score);
+    }
+  }
+}
+
+function updateTimer(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  gameTimer.innerHTML = `${minutes}:${seconds}`;
+}
+
+function stopGameTimer() {
+  clearInterval(timer);
+}
+
+function showTimer() {
+  gameTimer.style.visibility = "visible";
+}
+
+function pauseButton() {
+  gameBtn.style.visibility = "visible";
+}
+
+function hideGameButton() {
+  gameBtn.style.visibility = "hidden";
+}
+
+function showPopUp(text) {
+  popUpMsg.innerText = text;
+  popUp.classList.remove("pop-up--hide");
+}
+
+function hidePopUp() {
+  popUp.classList.add("pop-up--hide");
+}
 
 function init() {
+  field.innerHTML = "";
   createItem();
 }
 
@@ -36,5 +136,3 @@ function createItem() {
 function randomNum(min, max) {
   return Math.random() * (max - min) + min;
 }
-
-init();
